769af746108c7792b28b528fecd1d8d06413b400
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-06-27 16:12:49 +0900

    feat(config): Add client bean definition parser

---
+++ src/main/java/io/vanslog/spring/data/meilisearch/config/JsonHandlerBuilder.java
@@ -0,0 +1,31 @@
+package io.vanslog.spring.data.meilisearch.config;
+
+import com.meilisearch.sdk.json.GsonJsonHandler;
+import com.meilisearch.sdk.json.JacksonJsonHandler;
+import com.meilisearch.sdk.json.JsonHandler;
+
+/**
+ * Builder class that creates a JSON handler for MeiliSearch client.
+ *
+ * @since 1.0.0
+ * @see JsonHandler
+ * @see GsonJsonHandler
+ * @see JacksonJsonHandler
+ * @author Junghoon Ban
+ */
+public enum JsonHandlerBuilder {
+  GSON {
+    @Override
+    public JsonHandler build() {
+      return new GsonJsonHandler();
+    }
+  },
+  JACKSON {
+    @Override
+    public JsonHandler build() {
+      return new JacksonJsonHandler();
+    }
+  };
+
+  public abstract JsonHandler build();
+}
