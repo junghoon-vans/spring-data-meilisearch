bffb9e2a9e4252d276d31c14b72cd6c229a18e82
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-06-29 21:20:51 +0900

    feat(core): Implement simple mapping context

---
+++ src/main/java/io/vanslog/spring/data/meilisearch/core/mapping/SimpleMeilisearchMappingContext.java
@@ -0,0 +1,27 @@
+package io.vanslog.spring.data.meilisearch.core.mapping;
+
+import org.springframework.data.mapping.context.AbstractMappingContext;
+import org.springframework.data.mapping.model.Property;
+import org.springframework.data.mapping.model.SimpleTypeHolder;
+import org.springframework.data.util.TypeInformation;
+
+/**
+ * Meilisearch specific {@link AbstractMappingContext} implementation.
+ *
+ * @since 1.0.0
+ * @author Junghoon Ban
+ */
+public class SimpleMeilisearchMappingContext extends
+		AbstractMappingContext<SimpleMeilisearchPersistentEntity<?>, MeilisearchPersistentProperty> {
+
+	@Override
+	protected <T> SimpleMeilisearchPersistentEntity<?> createPersistentEntity(TypeInformation<T> typeInformation) {
+		return new SimpleMeilisearchPersistentEntity<T>(typeInformation);
+	}
+
+	@Override
+	protected MeilisearchPersistentProperty createPersistentProperty(Property property,
+																	   SimpleMeilisearchPersistentEntity<?> owner, SimpleTypeHolder simpleTypeHolder) {
+		return new SimpleMeilisearchPersistentProperty(property, owner, simpleTypeHolder);
+	}
+}
