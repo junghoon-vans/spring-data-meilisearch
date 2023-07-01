61cff91665b76586524601b320a776fd85d4ac7a
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-07-01 15:35:35 +0900

    test(core): Use assertj instead of assertions

--- src/test/java/io/vanslog/spring/data/meilisearch/core/mapping/SimpleMeilisearchPersistentEntityTests.java
+++ src/test/java/io/vanslog/spring/data/meilisearch/core/mapping/SimpleMeilisearchPersistentEntityTests.java
@@ -1,27 +1,23 @@
 package io.vanslog.spring.data.meilisearch.core.mapping;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.vanslog.spring.data.meilisearch.entities.Movie;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.util.TypeInformation;
 
 /**
+ * Unit tests for {@link SimpleMeilisearchPersistentEntity}.
+ *
  * @author Junghoon Ban
  */
 public class SimpleMeilisearchPersistentEntityTests {
 
-  private SimpleMeilisearchPersistentEntity<Movie> entityPersistentEntity;
-
-  @BeforeEach
-  void setup() {
-    TypeInformation<Movie> entityTypeInfo = TypeInformation.of(Movie.class);
-    entityPersistentEntity = new SimpleMeilisearchPersistentEntity<>(entityTypeInfo);
-  }
-
   @Test
-  void testGetIndexUid() {
+  void shouldReturnIndexUid() {
+    TypeInformation<Movie> entityTypeInfo = TypeInformation.of(Movie.class);
+    SimpleMeilisearchPersistentEntity<Movie> entityPersistentEntity = new SimpleMeilisearchPersistentEntity<>(entityTypeInfo);
     String indexUid = entityPersistentEntity.getIndexUid();
-    Assertions.assertEquals("movie", indexUid);
+    assertThat(indexUid).isEqualTo("movie");
   }
 }
