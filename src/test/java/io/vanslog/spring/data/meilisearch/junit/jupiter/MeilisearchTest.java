a7d148afe4f4a9381b5ce57b0e17a614c0c796cf
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-07-25 16:15:59 +0900

    test(junit): update annotation for meilisearch test

--- src/test/java/io/vanslog/spring/data/meilisearch/junit/jupiter/MeilisearchTest.java
+++ src/test/java/io/vanslog/spring/data/meilisearch/junit/jupiter/MeilisearchTest.java
@@ -4,7 +4,10 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * Annotation to activate Meilisearch integration tests.
@@ -14,5 +17,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @ExtendWith(MeilisearchExtension.class)
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public @interface MeilisearchTest {
 }
