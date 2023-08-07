890cf18c7c38511ebfa38e4c001b45000acc45be
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-08-07 17:17:33 +0900

    test(junit): locking beforeAll

--- src/test/java/io/vanslog/spring/data/meilisearch/junit/jupiter/MeilisearchExtension.java
+++ src/test/java/io/vanslog/spring/data/meilisearch/junit/jupiter/MeilisearchExtension.java
@@ -1,5 +1,7 @@
 package io.vanslog.spring.data.meilisearch.junit.jupiter;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
@@ -10,8 +12,15 @@ import org.junit.jupiter.api.extension.ExtensionContext;
  */
 public class MeilisearchExtension implements BeforeAllCallback {
 
+    private final Lock initLock = new ReentrantLock();
+
     @Override
     public void beforeAll(ExtensionContext context) {
-        new MeilisearchConnection();
+        initLock.lock();
+        try {
+            new MeilisearchConnection();
+        } finally {
+            initLock.unlock();
+        }
     }
 }
