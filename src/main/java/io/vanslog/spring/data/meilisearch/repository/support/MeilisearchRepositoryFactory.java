759669daebe01b679a77b37766d31b45ddbd8259
Junghoon Ban <junghoon.ban@gmail.com>
2025-01-01 12:05:26 +0900

    Update copyright info.

--- src/main/java/io/vanslog/spring/data/meilisearch/repository/support/MeilisearchRepositoryFactory.java
+++ src/main/java/io/vanslog/spring/data/meilisearch/repository/support/MeilisearchRepositoryFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 the original author or authors.
+ * Copyright 2023-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,12 +15,12 @@
  */
 package io.vanslog.spring.data.meilisearch.repository.support;
 
-import io.vanslog.spring.data.meilisearch.core.MeilisearchOperations;
-
 import org.springframework.data.repository.core.EntityInformation;
 import org.springframework.data.repository.core.RepositoryInformation;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
+
+import io.vanslog.spring.data.meilisearch.core.MeilisearchOperations;
 
 /**
  * Factory to create {@link SimpleMeilisearchRepository} instances.
--- src/main/java/io/vanslog/spring/data/meilisearch/repository/support/SimpleMeilisearchRepository.java
+++ src/main/java/io/vanslog/spring/data/meilisearch/repository/support/SimpleMeilisearchRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 the original author or authors.
+ * Copyright 2023-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,6 @@
  */
 package io.vanslog.spring.data.meilisearch.repository.support;
 
-import com.meilisearch.sdk.SearchRequest;
-import io.vanslog.spring.data.meilisearch.core.MeilisearchOperations;
-import io.vanslog.spring.data.meilisearch.repository.MeilisearchRepository;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -30,6 +26,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.core.EntityInformation;
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
+
+import com.meilisearch.sdk.SearchRequest;
+
+import io.vanslog.spring.data.meilisearch.core.MeilisearchOperations;
+import io.vanslog.spring.data.meilisearch.repository.MeilisearchRepository;
 
 /**
  * Meilisearch specific repository implementation.
