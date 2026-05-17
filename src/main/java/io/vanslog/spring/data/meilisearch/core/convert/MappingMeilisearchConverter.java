db4f2b6d04f82fc60a92004c0479b840a0d37cab
Junghoon Ban <junghoon.ban@gmail.com>
2026-05-17 21:25:34 +0900

    Add Meilisearch document conversion model.
    Introduce a map-shaped document boundary for entity conversion.
    
    Expand MappingMeilisearchConverter read/write support.
    
    Cover conversion behavior with focused unit tests.

--- src/main/java/io/vanslog/spring/data/meilisearch/core/convert/MappingMeilisearchConverter.java
+++ src/main/java/io/vanslog/spring/data/meilisearch/core/convert/MappingMeilisearchConverter.java
@@ -15,10 +15,16 @@
  */
 package io.vanslog.spring.data.meilisearch.core.convert;
 
+import io.vanslog.spring.data.meilisearch.core.document.MeilisearchDocument;
 import io.vanslog.spring.data.meilisearch.core.mapping.MeilisearchPersistentEntity;
 import io.vanslog.spring.data.meilisearch.core.mapping.MeilisearchPersistentProperty;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
@@ -28,9 +34,16 @@ import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.core.convert.support.GenericConversionService;
 import org.springframework.data.convert.CustomConversions;
+import org.springframework.data.mapping.PersistentPropertyAccessor;
+import org.springframework.data.mapping.PropertyHandler;
+import org.springframework.data.mapping.context.AbstractMappingContext;
 import org.springframework.data.mapping.context.MappingContext;
+import org.springframework.data.mapping.model.EntityInstantiators;
+import org.springframework.data.mapping.model.PersistentEntityParameterValueProvider;
+import org.springframework.data.mapping.model.PropertyValueProvider;
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
 
 /**
  * {@link io.vanslog.spring.data.meilisearch.core.convert.MeilisearchConverter} Implementation based on
@@ -42,6 +55,7 @@ public class MappingMeilisearchConverter implements MeilisearchConverter, Applic
 
 	private final MappingContext<? extends MeilisearchPersistentEntity<?>, MeilisearchPersistentProperty> mappingContext;
 	private final GenericConversionService conversionService;
+	private final EntityInstantiators instantiators = new EntityInstantiators();
 	private CustomConversions conversions = new MeilisearchCustomConversions(Collections.emptyList());
 
 	@SuppressWarnings("unused, FieldCanBeLocal")
@@ -75,15 +89,236 @@ public class MappingMeilisearchConverter implements MeilisearchConverter, Applic
 		return this.conversionService;
 	}
 
+	@Override
+	public <R> R read(Class<R> type, MeilisearchDocument source) {
+
+		Assert.notNull(type, "Type must not be null");
+		Assert.notNull(source, "Source document must not be null");
+
+		if (conversions.hasCustomReadTarget(MeilisearchDocument.class, type)) {
+			R converted = conversionService.convert(source, type);
+			if (converted != null) {
+				return converted;
+			}
+		}
+
+		MeilisearchPersistentEntity<R> entity = getPersistentEntity(type);
+		R instance = instantiators.getInstantiatorFor(entity).createInstance(entity,
+				new PersistentEntityParameterValueProvider<>(entity, propertyValueProvider(source), null));
+		PersistentPropertyAccessor<R> accessor = entity.getPropertyAccessor(instance);
+
+		entity.doWithProperties((PropertyHandler<MeilisearchPersistentProperty>) property -> {
+			if (entity.isCreatorArgument(property)) {
+				return;
+			}
+			String fieldName = property.getFieldName();
+			if (!source.containsKey(fieldName)) {
+				return;
+			}
+			Class<?> componentType = property.isCollectionLike() ? property.getComponentType() : property.getActualType();
+			accessor.setProperty(property, readValue(source.get(fieldName), property.getType(), componentType));
+		});
+
+		return instance;
+	}
+
+	@SuppressWarnings("unchecked")
+	private <R> MeilisearchPersistentEntity<R> getPersistentEntity(Class<R> type) {
+		return (MeilisearchPersistentEntity<R>) mappingContext.getRequiredPersistentEntity(type);
+	}
+
+	private PropertyValueProvider<MeilisearchPersistentProperty> propertyValueProvider(MeilisearchDocument source) {
+
+		return new PropertyValueProvider<MeilisearchPersistentProperty>() {
+			@Override
+			@SuppressWarnings("unchecked")
+			public <T> T getPropertyValue(MeilisearchPersistentProperty property) {
+				String fieldName = property.getFieldName();
+				Class<?> componentType = property.isCollectionLike() ? property.getComponentType()
+						: property.getActualType();
+				return (T) readValue(source.get(fieldName), property.getType(), componentType);
+			}
+		};
+	}
+
+	@Override
+	public void write(Object source, MeilisearchDocument sink) {
+
+		Assert.notNull(source, "Source object must not be null");
+		Assert.notNull(sink, "Sink document must not be null");
+
+		MeilisearchPersistentEntity<?> entity = mappingContext.getRequiredPersistentEntity(source.getClass());
+		PersistentPropertyAccessor<?> accessor = entity.getPropertyAccessor(source);
+
+		entity.doWithProperties((PropertyHandler<MeilisearchPersistentProperty>) property -> {
+			Object value = accessor.getProperty(property);
+			if (value != null) {
+				sink.put(property.getFieldName(), writeValue(value));
+			}
+		});
+	}
+
+	@Nullable
+	private Object writeValue(@Nullable Object value) {
+
+		if (value == null) {
+			return null;
+		}
+
+		if (conversions.hasCustomWriteTarget(value.getClass(), MeilisearchDocument.class)) {
+			Object converted = conversionService.convert(value, MeilisearchDocument.class);
+			if (converted != null) {
+				return converted;
+			}
+		}
+
+		if (isSimpleType(value.getClass())) {
+			return value;
+		}
+
+		if (value instanceof Collection<?>) {
+			Collection<?> collection = (Collection<?>) value;
+			Collection<Object> converted = new ArrayList<>(collection.size());
+			for (Object element : collection) {
+				converted.add(writeValue(element));
+			}
+			return converted;
+		}
+
+		if (value.getClass().isArray()) {
+			int length = Array.getLength(value);
+			Collection<Object> converted = new ArrayList<>(length);
+			for (int i = 0; i < length; i++) {
+				converted.add(writeValue(Array.get(value, i)));
+			}
+			return converted;
+		}
+
+		if (value instanceof Map<?, ?>) {
+			Map<?, ?> map = (Map<?, ?>) value;
+			Map<String, Object> converted = new LinkedHashMap<>(map.size());
+			map.forEach((key, mapValue) -> converted.put(String.valueOf(key), writeValue(mapValue)));
+			return converted;
+		}
+
+		MeilisearchDocument document = MeilisearchDocument.create();
+		write(value, document);
+		return document;
+	}
+
+	@Nullable
+	private Object readValue(@Nullable Object value, Class<?> targetType, Class<?> componentType) {
+
+		if (value == null) {
+			return null;
+		}
+
+		if (conversions.hasCustomReadTarget(value.getClass(), targetType)) {
+			Object converted = conversionService.convert(value, targetType);
+			if (converted != null) {
+				return converted;
+			}
+		}
+
+		if (Collection.class.isAssignableFrom(targetType) && value instanceof Collection<?>) {
+			Collection<?> collection = (Collection<?>) value;
+			Collection<Object> converted = new ArrayList<>(collection.size());
+			Class<?> elementType = componentType != null ? componentType : Object.class;
+			for (Object element : collection) {
+				converted.add(readCollectionElement(element, elementType));
+			}
+			return converted;
+		}
+
+		if (ClassUtils.isAssignableValue(targetType, value)) {
+			return value;
+		}
+
+		if (conversionService.canConvert(value.getClass(), targetType)) {
+			Object converted = conversionService.convert(value, targetType);
+			if (converted != null) {
+				return converted;
+			}
+		}
+
+		if (value instanceof MeilisearchDocument) {
+			MeilisearchDocument document = (MeilisearchDocument) value;
+			return read(targetType, document);
+		}
+
+		if (value instanceof Map<?, ?>) {
+			Map<?, ?> map = (Map<?, ?>) value;
+			return read(targetType, toDocument(map));
+		}
+
+		return value;
+	}
+
+	@Nullable
+	private Object readCollectionElement(@Nullable Object element, Class<?> componentType) {
+
+		if (element == null || ClassUtils.isAssignableValue(componentType, element)) {
+			return element;
+		}
+
+		if (conversions.hasCustomReadTarget(element.getClass(), componentType)) {
+			Object converted = conversionService.convert(element, componentType);
+			if (converted != null) {
+				return converted;
+			}
+		}
+
+		if (element instanceof MeilisearchDocument) {
+			MeilisearchDocument document = (MeilisearchDocument) element;
+			return read(componentType, document);
+		}
+
+		if (element instanceof Map<?, ?>) {
+			Map<?, ?> map = (Map<?, ?>) element;
+			return read(componentType, toDocument(map));
+		}
+
+		return element;
+	}
+
+	private boolean isSimpleType(Class<?> type) {
+		return conversions.getSimpleTypeHolder().isSimpleType(type);
+	}
+
+	private void updateMappingContextSimpleTypeHolder() {
+
+		if (mappingContext instanceof AbstractMappingContext<?, ?>) {
+			setSimpleTypeHolder(mappingContext, conversions);
+		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	private static void setSimpleTypeHolder(
+			MappingContext<? extends MeilisearchPersistentEntity<?>, MeilisearchPersistentProperty> mappingContext,
+			CustomConversions conversions) {
+
+		AbstractMappingContext abstractMappingContext = (AbstractMappingContext) mappingContext;
+		abstractMappingContext.setSimpleTypeHolder(conversions.getSimpleTypeHolder());
+	}
+
+	private static MeilisearchDocument toDocument(Map<?, ?> source) {
+
+		MeilisearchDocument document = MeilisearchDocument.create();
+		source.forEach((key, value) -> document.put(String.valueOf(key), value));
+		return document;
+	}
+
 	public void setConversions(CustomConversions conversions) {
 
 		Assert.notNull(conversions, "CustomConversions must not be null");
 
 		this.conversions = conversions;
+		updateMappingContextSimpleTypeHolder();
 	}
 
 	@Override
 	public void afterPropertiesSet() {
+		updateMappingContextSimpleTypeHolder();
 		conversions.registerConvertersIn(conversionService);
 	}
 }
